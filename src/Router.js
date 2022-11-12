e206b6f7a58c04793a0e2f9d6473e02df35b3769
HolyWatter <qktjdtn97@icloud.com>
2022-11-13 01:38:11 +0900

    타입스크립트 연습

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage.tsx";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
 };
+
+export default Router;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainPage = () => {
+  return <div>asdfd</div>;
+};
+
+export default MainPage;
