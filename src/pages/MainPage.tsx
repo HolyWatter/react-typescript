fba53794b0c11586fcc9e81d4a06d478e0352f17
HolyWatter <qktjdtn97@icloud.com>
2022-11-13 16:32:08 +0900

    임시저장

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const MainPage = () => {
+  function newFunc(x: number, y: number): number {
+    return x + y;
+  }
+
+  const oldFunc = (x: number, y: number): void => {
+    x + y;
+  };
+
   return <div>asdfd</div>;
 };
 
